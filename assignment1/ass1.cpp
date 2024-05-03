e9ab2b367a0217e64a705769b0df9c440658774a
Aditya-raj123 <130673373+Aditya-raj123@users.noreply.github.com>
2024-05-03 13:38:27 +0530

    Add files via upload
    solved assignment 1 is uploaded

---
+++ assignment1/ass1.cpp
@@ -0,0 +1,12 @@
+// How can you output “Physics”and
+//   “Wallah” in two different lines in C++?
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"physics "<<endl<<"wallah";
+    
+    return 0;
+
+}
+
