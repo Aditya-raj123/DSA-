c72bdcfbc5a6dc743473d4b3d047b697077ab7f6
Aditya-raj123 <130673373+Aditya-raj123@users.noreply.github.com>
2024-05-10 14:38:45 +0530

    Add files via upload
    pointer assignmnet

---
+++ pointerasssignmnet/assignment1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    int y;
+    int *p1=&x;
+     int *p2=&y;
+     cout<<"enter the 1st number";
+     cin>>*p1;
+     cout<<"enter the 2nd number";
+     cin>>*p2;
+     cout<<*p1 * *p2;
+         return 0;
+
+}
