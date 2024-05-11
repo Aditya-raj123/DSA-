43e421f3f4876fe9a4a7b79b71c26f45df547a45
Aditya-raj123 <130673373+Aditya-raj123@users.noreply.github.com>
2024-05-11 14:27:29 +0530

    Add files via upload
    array 1 assignmnet

---
+++ assignmnet2.cpp
@@ -0,0 +1,16 @@
+// Find the second largest element in the given Array in one pass.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the size of the array";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<=n-1;i++){
+        cin>>arr[i];
+    }
+    
+    return 0;
+
+
+}
