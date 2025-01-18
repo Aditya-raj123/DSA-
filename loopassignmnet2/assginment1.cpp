951db3c4bc9c29027478256dcfdf6edc78b73378
Aditya-raj123 <130673373+Aditya-raj123@users.noreply.github.com>
2025-01-18 22:18:42 +0530

    Delete loopassignmnet2 directory

--- loopassignmnet2/assginment1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    while('1'<'2'){
-        cout<<"in a while loop"<<endl;
-    }
-    return 0;
-
-}
--- loopassignmnet2/assginment3.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    for(int x=1;x*x<=10;x++){
-        cout<<"in for loop"<<endl;
-    }
-    return 0;
-
-}
